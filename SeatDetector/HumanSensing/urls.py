63963d47d42f5d8d895a883439f940a133d9e287
Hanscat <hanscat@icloud.com>
2017-10-30 14:34:34 -0400

    add bootstrap, remain problems of including css and js

--- SeatDetector/HumanSensing/urls.py
+++ SeatDetector/HumanSensing/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path
+from django.conf.urls.static import static
 from django.conf.urls import include, url
+from django.conf import settings
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'), 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ SeatDetector/static/js/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Kefan on 10/20/17.
+ */
+$( function() {
+  var $winHeight = $( window ).height();
+  $( '.container' ).height( $winHeight );
+});
