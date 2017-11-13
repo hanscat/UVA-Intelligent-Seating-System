96b15072de77d89fff97471ac329d098bf958245
Hanscat <hanscat@icloud.com>
2017-11-12 21:03:57 -0500

    fix bug from merge

--- SeatDetector/HumanSensing/views.py
+++ SeatDetector/HumanSensing/views.py
@@ -7,37 +7,37 @@ from HumanSensing import subscribe
 
 
 def index(request):
-    # return HttpResponse("Hello, world. You're at Dr. Fourier's project index.")
+	# return HttpResponse("Hello, world. You're at Dr. Fourier's project index.")
 
-    topics = ['uvafourier']
-	#topics = ['#']
+	topics = ['uvafourier']
+
 
 	topic = "yo"
 	payload = "hmmm"
 
 	"""
 	while(1):
-	    m = subscribe.simple(topics, hostname="iot.eclipse.org", retained=False, msg_count=4)
-	    for a in m:
-	        print("Topic: " + (a.topic))
-	        print("Payload: " + str(a.payload))
-    """
+		m = subscribe.simple(topics, hostname="iot.eclipse.org", retained=False, msg_count=4)
+		for a in m:
+			print("Topic: " + (a.topic))
+			print("Payload: " + str(a.payload))
+	"""
+	# 
+	# m = subscribe.simple(topics, hostname="iot.eclipse.org", retained=False, msg_count=4)
+	# 
+	# for a in m:
+	# 	topic = a.topic
+	# 	payload = a.payload.split(",")[0]
+	# 
 
-	m = subscribe.simple(topics, hostname="iot.eclipse.org", retained=False, msg_count=4)
-
-	for a in m:
-		topic = a.topic
-		payload = a.payload.split(",")[0];
-
-
-    
+	
 	element = {
 		'topic' : topic,
 		'payload' : payload
 	}
 
-    #return render(request, 'index.html')
+	#return render(request, 'index.html')
 	return render(request, 'index.html', element)
 
 def test(request):
-    return render(request, 'index.html')
+	return render(request, 'index.html')
